5f4a337ee027af0c7675bb0e377c9851915e8795
distro104 <distro104@gmail.com>
2020-12-01 10:56:39 -0300

    Complete the Django TEMPLATE tutrorial

--- python/django/exercise_1/exercise_1/exercise_1_app/views.py
+++ python/django/exercise_1/exercise_1/exercise_1_app/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    my_dict = {'inside_me': "Hello i m from views.py!"}
-    return render(request, 'index.html', context=my_dict)
+    my_dict = {"insert_me": "Hello i m from views.py!"}
+    return render(request, 'exercise_1_app/index.html', context=my_dict)
