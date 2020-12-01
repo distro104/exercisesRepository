a42fa393f24eaa736e88f1eb82237ffe32a68d51
distro104 <distro104@gmail.com>
2020-12-01 10:16:16 -0300

    added new python exercise using django

--- python/django/exercise_1/exercise_1/exercise_1/urls.py
+++ python/django/exercise_1/exercise_1/exercise_1/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
 from exercise_1_app import views
 
 urlpatterns = [
+    url(r'^exercise_1_app/', include('exercise_1_app.urls')),
     url(r'^$', views.index, name='index'),
     url(r'admin/', admin.site.urls),
 ]
--- python/django/exercise_1/exercise_1/exercise_1_app/views.py
+++ python/django/exercise_1/exercise_1/exercise_1_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Canada i'll be there!!!!")
+    my_dict = {'inside_me': "Hello i m from views.py!"}
+    return render(request, 'index.html', context=my_dict)
